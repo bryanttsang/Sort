4551646ae9186e93ed354145a2334a12c54f576f
bryanttsang <wtsang1931@bths.edu>
2019-01-06 20:59:21 -0500

    BubbleSort v1

--- src/BubbleSortRunner.java
+++ src/BubbleSortRunner.java
@@ -3,7 +3,7 @@ public class BubbleSortRunner
     public static void main(String args[])
     {
         //create random array
-        int[] array = BubbleSort.randIntArr(10);
+        int[] array = SortingUtil.randIntArr(10);
 
         //print initial array
         System.out.println("Before: ");
@@ -12,12 +12,17 @@ public class BubbleSortRunner
         System.out.println();
 
         //sort with bubble sort
-        BubbleSort.bubbleSort(array);
+        long time = System.nanoTime();
+        SortingUtil.bubbleSort(array);
+        time = System.nanoTime() - time;
 
         //print final array
         System.out.println("After: ");
         for (int num:array)
             System.out.println(num + " ");
         System.out.println();
+
+        //print time
+        System.out.println("Time taken: " + time + " nanoseconds");
     }
 }
--- src/SortingUtil.java
+++ src/SortingUtil.java
@@ -1,20 +1,13 @@
-public class BubbleSort
+public class SortingUtil
 {
-    public static void Swap(int[] array, int x, int y)
-    {
-        int temp = array[x];
-        array[x] = array[y];
-        array[y] = temp;
-    }
-
     public static void bubbleSort(int[] array)
     {
         int swapNum = -1;
         while (swapNum != 0)
         {
+            swapNum = 0;
             for (int i = 0; i < array.length - 1; i++)
             {
-                swapNum = 0;
                 int j = i + 1;
                 if (array[i] > array[j])
                 {
@@ -23,6 +16,13 @@ public class BubbleSort
                 }
             }
         }
+    }
+
+    public static void Swap(int[] array, int x, int y)
+    {
+        int temp = array[x];
+        array[x] = array[y];
+        array[y] = temp;
     }
 
     public static int[] randIntArr(int length)
